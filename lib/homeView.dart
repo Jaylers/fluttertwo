fc862429e1be3c4e37bd06bb9ae3e736a2947b4e
Sapthawee Srichomthong <Mypandacm@hotmail.co.th>
2018-07-21 17:12:32 +0700

    add ignore. edit home View, add dashBoard

--- lib/homeView.dart
+++ lib/homeView.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:fluttertwo/dashBoardView.dart';
 
 class HomeView extends StatefulWidget {
   @override
@@ -13,11 +14,11 @@ class _HomeView extends State<HomeView>{
   }
 }
 
-class _Message extends StatelessWidget {
+class _Message extends StatelessWidget { // ignore: must_be_immutable
 
   Widget author = Container(
     margin: EdgeInsets.all(20.0),
-    child: Text("Jaylers Infinit", style: TextStyle(fontSize: 20.0, color: Colors.white)),
+    child: Text("Jaylers Infinite", style: TextStyle(fontSize: 20.0, color: Colors.white)),
   );
 
   Widget welcome = Container(
@@ -25,11 +26,42 @@ class _Message extends StatelessWidget {
     child: Text("Hello, world", style: TextStyle(fontSize: 20.0, color: Colors.blue)),
   );
 
-  Widget button = RaisedButton(onPressed: (){ getIncNum(24);}, color: Colors.green, child: Text("Press Me!"),);
-  
   @override
   Widget build(BuildContext context){
+
+    Widget button = RaisedButton(onPressed: (){ nevigator(context);}, color: Colors.green, child: Text("OK"), colorBrightness: Brightness.light,);
+
+    Widget stBtn = Container(
+      padding: EdgeInsets.only(left: 5.0, right: 5.0),
+      child: RaisedButton(onPressed: (){}, color: Colors.green, child: Text("Home"),),
+      width: 0.25 * (MediaQuery.of(context).size.width),
+    );
+
+    Widget ndBtn = Container(
+      padding: EdgeInsets.only(left: 5.0, right: 5.0),
+      child: RaisedButton(onPressed: (){}, color: Colors.green, child: Text("Me"),),
+      width: 0.25 * (MediaQuery.of(context).size.width),
+    );
+
+    Widget rdBtn = Container(
+      padding: EdgeInsets.only(left: 5.0, right: 5.0),
+      child: RaisedButton(onPressed: (){}, color: Colors.green, child: Text("Favorit"),),
+      width: 0.25 * (MediaQuery.of(context).size.width),
+    );
+
+    Widget thBtn = Container(
+      padding: EdgeInsets.only(left: 5.0, right: 5.0),
+      child: RaisedButton(onPressed: (){}, color: Colors.green, child: Text("Profile"),),
+      width: 0.25 * (MediaQuery.of(context).size.width),
+    );
+
+
     Widget image = Image.asset("assets/flutter.png", width: MediaQuery.of(context).size.width,);
+
+    Widget formGroup = Row(
+      children: <Widget>[stBtn, ndBtn, rdBtn, thBtn],
+    );
+
     return new MaterialApp(
       title: "Message",
       home: new Scaffold(
@@ -38,14 +70,18 @@ class _Message extends StatelessWidget {
         ),
         body: Scaffold(
           body: Column(
-            children: <Widget>[image, welcome, button],
+            children: <Widget>[image, formGroup, welcome, button],
           )
         )
       ),
     );
   }
 
-  static getIncNum(num){
-    print("I'm $num");
+  static nevigator(context){
+    print("Navigate to dashboard");
+//    Navigator.of(context)
+//    .push(new CupertinoPageRoute<void>(
+//      builder: (BuildContext cont) => new dashBoardView(),
+//      fullscreenDialog: true));
   }
 }
